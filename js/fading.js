9eeca24360bac48d3e7ef6f3dc6c741eec2e51c3
SeverinJB <32867365+SeverinJB@users.noreply.github.com>
2019-04-09 21:34:05 +0200

    V0.9.9 Webpage

---
+++ js/fading.js
@@ -0,0 +1,26 @@
+var visited = sessionStorage.getItem('visit');
+
+if (document.location.href != sessionStorage.getItem('lastPage')) {
+  $(document).getElementsByClassName('collage').innerHTML = 'Hello JavaScript!';
+}
+
+if (visited == null || document.location.href == sessionStorage.getItem('lastPage')) {
+  $.fn.fadingin = function(fadein) {
+    this.css("display", "none");
+    this.fadeIn(fadein);
+  };
+
+  $.fn.fadingout = function(fadeout) {
+    this.fadeOut(fadeout);
+  };
+
+  $(document).ready(function() {
+    $(".collage").fadingout(4000, 12000);
+  });
+
+  $(document).ready(function() {
+    $(".fade-content").fadingin(7000, 1000);
+  });
+  sessionStorage.setItem('visit', 1);
+}
+sessionStorage.setItem('lastPage', document.location.href);
