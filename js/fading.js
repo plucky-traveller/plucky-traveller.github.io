aba658fa20155729cc9d621d22ee702324a2e9a0
delfimpandiani <44606644+delfimpandiani@users.noreply.github.com>
2019-04-09 22:40:15 +0200

    Update fading.js

--- js/fading.js
+++ js/fading.js
@@ -1,5 +1,6 @@
 var visited = sessionStorage.getItem('visit');
 if (visited === null) {
+  console.log("loop");
   $.fn.fadingin = function(fadein) {
     this.css("display", "none");
     this.fadeIn(fadein);
@@ -12,7 +13,7 @@ if (visited === null) {
   $(document).ready(function() {
     $(".collage").fadingout(4000, 12000);
   });
-
+  
   $(document).ready(function() {
     $(".fade-content").fadingin(7000, 1000);
   });
